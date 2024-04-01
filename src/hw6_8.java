fd064f378d6366943651cf56302a6901cd4d0a4c
1376524890 <plk161211@163.com>
2024-04-01 14:48:53 +0800

    6.8完成

--- src/hw6_8.java
+++ src/hw6_8.java
@@ -8,7 +8,8 @@ public class hw6_8 {
     }
 
     public static void main(String[] args) {
-        System.out.println("摄氏度\t华氏度\t华氏度\t摄氏度\t");
+        System.out.println("摄氏度\t华氏度\t\t华氏度\t摄氏度\t");
+        System.out.println("----------------------------------");
         for(double celsius = 40.0, fahrenheit = 120.0;celsius>30;celsius-=1,fahrenheit-=10){
             System.out.println(celsius+"\t"+(double)((int)(celsiusToFahrenheit(celsius)*10))/10+"\t\t"+fahrenheit+"\t"+(double)((int)(fahrenheitToCelsius(fahrenheit)*100))/100);
         }
