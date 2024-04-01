fa4220130ddbe3ce31cbbb475ed7191f5ddd7674
1376524890 <plk161211@163.com>
2024-04-01 14:23:20 +0800

    创建完成

--- src/hw6_2.java
+++ src/hw6_2.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
 public class hw6_2 {
+    public static int sumDigits(long n){
+        long devide = 10;
+        int sum = 0;
+        int timeMax;
+        long n_temp = n;
+        for(timeMax=1;n_temp>10;n_temp/=10,timeMax++);
+        for(int time = 0;time<timeMax;time++){
+            sum+=(n%devide);
+            n/=10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextLong();
+        System.out.println(sumDigits(n));
+    }
 }
