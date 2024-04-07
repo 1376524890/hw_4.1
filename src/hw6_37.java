b94e60e36b1f5820bba96c392a3e1c84a85518fc
1376524890 <plk161211@163.com>
2024-04-07 20:20:55 +0800

    6.37输出乱码，使用sb前备份

--- src/hw6_37.java
+++ src/hw6_37.java
@@ -1,31 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
 public class hw6_37 {
-    public static String format(int number, int width){
+    public static String format(String number, int width){
         char[] fill_0 = new char[width];
-        int digit,number_temp=number;
-        for(digit = 1;number_temp>=10;number_temp/=10,digit++);
-        char[] num = new char[digit];
-        int ten = 10;
-        number_temp = number;
-        for(int i = digit-1;i>=0;i--){
-            num[i] = (char)(number_temp % ten);
-            ten*=10;
-        }
-        String num_str = Arrays.toString(num);
+        int digit = number.length();
         String fill0_str = "";
         if(width>=digit){
             for (int i = 0; i < width-digit; i++) {
                 fill_0[i] = '0';
             }
-            fill0_str = Arrays.toString(fill_0);
+            fill0_str = new String(fill_0);
         }
-        return fill0_str.concat(num_str);
+        return fill0_str + number;
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
+        String number = scan.next();
         int width = scan.nextInt();
         System.out.println(format(number,width));
     }
