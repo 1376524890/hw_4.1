22a10856837d28f98977a1264a1714df6541937c
1376524890 <plk161211@163.com>
2024-04-08 00:56:46 +0800

    6.37最终版本，改对了

--- src/hw6_37.java
+++ src/hw6_37.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class hw6_37 {
     public static String format(String number, int width){
-        char[] fill_0 = new char[width];
         int digit = number.length();
+        char[] fill_0 = new char[width-digit];
         String fill0_str = "";
         if(width>=digit){
             for (int i = 0; i < width-digit; i++) {
