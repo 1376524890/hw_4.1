1e1f70c00c8566daf31d1a8a7bd73ab54ea529ea
1376524890 <plk161211@163.com>
2024-04-07 18:31:05 +0800

    6.28完成

--- src/hw6_28.java
+++ src/hw6_28.java
@@ -1,36 +1,28 @@
 public class hw6_28 {
     public static boolean isPrime(int n){
-        for(int i = 2;i<(int)(hw6_22.sqrt(n,1)+1);i++){
-            if(n%2==0){
+        if(n == 1){
+            return false;
+        }
+        for(int i = 2;i<n;i++){
+            if(n%i==0){
                 return false;
             }
         }
         return true;
     }
-    public static int nextPrime(int n){
-        n += 1;
-        for(;!isPrime(n);n++);
-        return n;
-    }
 
     public static void main(String[] args) {
-        boolean[] prime = new boolean[100000];
-        for(int n = 0; n < 100000; n++){
-            if(isPrime(n)){
-                prime[n] = true;
+        int num = 1;
+        for(int p = 1;p<=31;p++){
+            num = 1;
+            for(int i=0;i<p;i++){
+                num*=2;
             }
-            else {
-                prime[n] = false;
-            }
-        }
-        for(int p = 2, num =2;p<32;p++){
-            for(int i = 0;i<p;i++){
-                num *= 2;
-            }
-            num -= 1;
-            if(prime[num]){
+            num--;
+            if(isPrime(num)){
                 System.out.println(p+"\t\t"+num);
             }
         }
+
     }
 }
